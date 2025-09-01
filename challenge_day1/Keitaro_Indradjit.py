4b63c223ec635480ae36851006649bc637886232
keitaro-jpg <keitaroindradjit@gmail.com>
2025-09-01 17:40:49 +0700

    Update name in function

--- challenge_day1/Keitaro_Indradjit.py
+++ challenge_day1/Keitaro_Indradjit.py
@@ -1,2 +1,23 @@
 def get_name(): 
     return "Keitaro Indradjit" 
+    
+def get_paragraph_act1():
+    paragraph = (
+        "A single, brilliant blue light flickered to life on the robot's chest, "
+        "cutting through the dim workshop. A collective gasp escaped from Leo, "
+        "Moos, Jia, Finn, Zara, and Kai, as hope, bright and powerful as the light itself, filled the room."
+    )
+    return paragraph
+
+def get_paragraph_act2():
+    paragraph = (
+        "A speaker grill on the robot’s face crackled, emitting a burst of static "
+        "that made Finn jump. It was trying to speak, to bridge the gap between machine "
+        "and maker, a desire understood by Leo, Moos, Jia, Zara, and Kai."
+    )
+
+def get_paragraph_act3():
+    paragraph = (
+        "The robot, U-i-i-a-i-i, extended its newly tested hand. It wasn't a command "
+        "or a random movement; it was a clear gesture of connection, an offering to Leo, Moos, Jia, Finn, Zara, and Kai."
+    )

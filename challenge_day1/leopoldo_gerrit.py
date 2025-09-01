10cebfdc741938d57dcd85a24fa39e851b61c983
Gerrit <gerrit.s.xd@gmail.com>
2025-09-01 12:28:21 +0200

    1

--- challenge_day1/leopoldo_gerrit.py
+++ challenge_day1/leopoldo_gerrit.py
@@ -10,4 +10,4 @@ def get_paragraph_act2():
     return "The static suddenly cleared, replaced by a series of pure, distinct tones that echoed through the workshop. The sound was beautiful, a complex melody that captivated Leo, Moos, Jia, Finn, Zara, and Kai and promised something more than mere words."
 
 def get_paragraph_act3():
-    return "A new era had dawned in their small workshop, its arrival announced by the sound "U-i-i-a-i-i." It was the beginning of a future that Leo, Moos, Jia, Finn, Zara, and Kai would now build together with their incredible creation."
+    return 'A new era had dawned in their small workshop, its arrival announced by the sound "U-i-i-a-i-i." It was the beginning of a future that Leo, Moos, Jia, Finn, Zara, and Kai would now build together with their incredible creation.'

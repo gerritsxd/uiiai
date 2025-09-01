62fd488e423115184e2a88bcfc943485807b3d19
MHaenen <haenenmies@gmail.com>
2025-09-01 11:13:47 +0200

    Updated  it so that Mies_Haenen is also added

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -4,6 +4,7 @@ def print_team_members():
     team_name = "uiiai"  # You can change this to your actual team name
     print(f"This is Team {team_name}. We are:")
     print(f"- {leopoldo_gerrit.get_name()}")
-
+    print(f"- {Mies_Haenen.get_name()}")
+    
 if __name__ == "__main__":
     print_team_members()

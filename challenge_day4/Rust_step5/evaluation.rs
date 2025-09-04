968e08767a32a450b40da40c9c839b8171d3f101
Gerrit <gerrit.s.xd@gmail.com>
2025-09-04 10:19:26 +0200

    Rust G

--- challenge_day4/Rust_step5/evaluation.rs
+++ challenge_day4/Rust_step5/evaluation.rs
@@ -5,19 +5,19 @@ use std::fs::OpenOptions;
 use std::io::Write;
 
 fn main() -> io::Result<()> {
-    let path = Path::new("data5.txt");
+        let path = Path::new("../testdata/data5.txt");
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
     
     let mut output = OpenOptions::new()
         .write(true)
         .create(true)
-        .open("data6.txt")?;
+                .open("../testdata/data6.txt")?;
     
     for (indexing, line) in reader.lines().enumerate() {
         let line = line?;
         
-        if index == 0 {
+                if indexing == 0 {
             // This is the header, add "Evaluation" to it and write to file
             writeln!(output, "{},Evaluation", line)?;
             continue;

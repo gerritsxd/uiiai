79eb6944e9cc20fb3457a8e9d841702bac9ba922
MHaenen <haenenmies@gmail.com>
2025-09-04 13:08:56 +0200

    Julia solved and corrected

--- challenge_day4/Julia_step3/sort.jl
+++ challenge_day4/Julia_step3/sort.jl
@@ -1,8 +1,8 @@
 using DataFrames, CSV, Statistics, DelimitedFiles
 
 # Read the CSV file into a DataFrame
-people_df = CSV.File("data3.csv") |> DataFrame
-
+data_path = joinpath(@__DIR__, "..", "testdata", "data3.csv")
+people_df = CSV.File(data_path) |> DataFrame
 # Function to classify a score based on quartiles
 function classify_score(score, quartiles)
     if score <= quartiles[1]
@@ -32,7 +32,7 @@ for col_name in names(people_df)[2:end]
     quartiles = quantile(valid_data, [0.25, 0.5, 0.75])
 
     # Replace values with categories or 'low' if they are Float64
-    new_col = map(x -> x isa Float64 ? "low" : classification_score(x, quartiles), col_data)
+    new_col = map(x -> x isa Float64 ? "low" : classify_score(x, quartiles), col_data)
     people_df[!, col_name] = new_col
 end
 
